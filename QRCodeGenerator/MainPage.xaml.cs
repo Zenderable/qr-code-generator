c60e531b2fbf0e8c9a277c0e2aeb5e166b3e7f90
adampszczola <adampszczola98@gmail.com>
2020-12-30 00:18:00 +0100

    Add QR generation from API

--- QRCodeGenerator/MainPage.xaml.cs
+++ QRCodeGenerator/MainPage.xaml.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Net;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Storage.Streams;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -14,6 +16,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
+using Windows.Storage;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -24,10 +27,12 @@ namespace QRCodeGenerator
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        int size = 500;
+        string selected = "png", color, url;
         public MainPage()
         {
             this.InitializeComponent();
-
+           
         }
 
         private void Generate_Click(object sender, RoutedEventArgs e)
@@ -46,7 +51,59 @@ namespace QRCodeGenerator
 
         private void ListBoxFormat_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            var selected = lbFormat.SelectedValue;
+             selected = lbFormat.SelectedValue.ToString();
+        }
+
+        private void btnSetSize_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbSetSize.Text != "") 
+                size = int.Parse(tbSetSize.Text);
+            if (size < 100)
+            {
+                size = 100;
+                tbSetSize.Text = size.ToString();
+            } 
+            if (size > 1000)
+            {
+                size = 1000;
+                tbSetSize.Text = size.ToString();
+            }
+                
+        }
+
+        private void cpColorPicked_ColorChanged(ColorPicker sender, ColorChangedEventArgs args)
+        {
+            color = cpColorPicked.Color.ToString().Remove(0, 3);
+            
+        }
+
+        private void tbSetSize_BeforeTextChanging(TextBox sender, TextBoxBeforeTextChangingEventArgs args)
+        {
+            args.Cancel = args.NewText.Any(c => !char.IsDigit(c));
+        }
+
+        private void btnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            var content = WebUtility.UrlEncode(tbQRData.Text);
+            url = "http://api.qrserver.com/v1/create-qr-code/?data=" + content + "&size=" + size + "x" + size + "&color=" + color + "&format=" + selected;
+            imgGenerated.Source = new BitmapImage(new Uri(url));
+            
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+                       
+            //string localfolder = ApplicationData.Current.LocalFolder.Path;
+            //var array = localfolder.Split('\\');
+            //var username = array[2];
+            //var path = tbQRTitle.Text + "." + selected;
+            WebClient webClient = new WebClient();
+            var path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\asd.png"; 
+
+
+            //tu wyrzuca zawsze błąd odnośnie braku uprawnień do zapisywania, próbowałem odpalac jako admin, sprawdzałem różne miejsca zapisu i dupa, chyba znaleźć inny sposób na zapis
+            webClient.DownloadFile(url, path);
+            
         }
     }
 }
